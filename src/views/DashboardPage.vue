473acfcf770ace75444d98b15e8498aae927ebbe
Samuel Onasis <samuelonasis23@gmail.com>
2025-06-16 12:43:28 +0700

    fix: dashboard looks

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -3,6 +3,9 @@ import AddTransactionDialog from "../components/AddTransactionDialog.vue";
 import { onMounted, ref, watch } from "vue";
 import axiosInstance from "@/lib/axios_instance";
 import { ArrowDownTrayIcon, ArrowUpTrayIcon } from "@heroicons/vue/24/outline";
+import { useAuthStore } from "@/store/auth";
+
+const authStore = useAuthStore();
 
 const showAddTransactionDialog = ref(false);
 
@@ -199,101 +202,173 @@ onMounted(() => {
       @cancel="showAddTransactionDialog = false"
     />
 
-    <button
-      class="mb-4 self-end px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer"
-      @click="showAddTransactionDialog = true"
-    >
-      Add Transaction
-    </button>
-    <div
-      class="bg-white rounded-2xl shadow-lg flex flex-col max-h-[400px] py-6 pl-6 overflow-hidden mb-8"
-    >
-      <h2 class="mb-8 text-xl font-semibold text-gray-800">
-        Daily Transactions : {{ new Date().toLocaleDateString() }}
-      </h2>
-      <div
-        class="flex flex-col md:flex-row gap-8 h-full min-h-[200px] overflow-y-auto"
+    <div class="flex items-center justify-between mb-6 flex-wrap gap-4">
+      <div>
+        <h1 class="text-3xl font-bold text-gray-800">Dashboard</h1>
+        <p class="text-gray-600 mt-2">
+          Welcome back!
+          {{ authStore.user.username || "User" }},
+          <br />
+          Here’s a quick overview of your financial status.
+        </p>
+      </div>
+
+      <button
+        class="mb-4 self-end px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 cursor-pointer"
+        @click="showAddTransactionDialog = true"
       >
-        <div class="flex-1 pr-6 md:p-0">
-          <h3
-            class="text-lg font-semibold text-red-500 mb-4 flex items-center gap-2"
-          >
-            <ArrowUpTrayIcon class="w-5 h-5 inline text-red-500" />
-            Expenses
+        Add Transaction
+      </button>
+    </div>
+    <div
+      class="bg-white rounded-2xl shadow-lg flex flex-col max-h-[400px] pl-6 py-6 overflow-hidden mb-8"
+    >
+      <div class="mb-4 flex items-center gap-4 flex-wrap pr-6">
+        <h2 class="text-2xl font-bold text-gray-800 flex items-center gap-2">
+          <span class="inline-block w-3 h-3 rounded-full bg-blue-500"></span>
+          Transaksi Hari Ini
+        </h2>
+        <span
+          class="text-gray-500 text-base font-medium bg-gray-100 px-4 py-1 rounded-lg shadow-sm"
+        >
+          {{
+            new Date().toLocaleDateString("id-ID", {
+              weekday: "long",
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+            })
+          }}
+        </span>
+      </div>
+      <div class="flex flex-col h-full min-h-[200px]">
+        <div
+          class="justify-between gap-4 border-b pb-2 mb-4 border-gray-200 mr-6 hidden xl:flex"
+        >
+          <div class="flex-1 flex justify-between items-center gap-2">
+            <h3
+              class="text-lg font-semibold text-red-500 flex items-center gap-2"
+            >
+              <ArrowUpTrayIcon class="w-5 h-5 inline text-red-500" />
+              Expenses
+            </h3>
             <span class="ml-auto text-sm text-gray-400 font-normal"
               >-
               <span class="font-bold text-red-500"
                 >Rp{{ Number(dailyExpenseTotal).toLocaleString() }}</span
               ></span
             >
-          </h3>
-          <ul class="list-none p-0 m-0 divide-y divide-gray-100">
-            <li
-              v-for="item in dailyExpense"
-              :key="'expense-' + item.id"
-              class="flex justify-between py-2 items-center"
+          </div>
+          <div class="w-px bg-gray-200 mx-4" />
+          <div class="flex-1 flex items-center gap-2">
+            <h3
+              class="text-lg font-semibold text-green-600 flex items-center gap-2"
             >
-              <div>
-                <span class="text-gray-700 font-medium">{{
-                  item.nama_pengeluaran
-                }}</span>
-                <span class="block text-xs text-gray-400">{{
-                  new Date(item.tanggal).toLocaleDateString()
-                }}</span>
-              </div>
-              <span class="text-red-500 font-bold"
-                >- Rp{{
-                  Number(item.jumlah_pengeluaran).toLocaleString()
-                }}</span
-              >
-            </li>
-            <li
-              v-if="!dailyExpense.length"
-              class="py-2 text-gray-400 text-center"
-            >
-              No expenses
-            </li>
-          </ul>
+              <ArrowDownTrayIcon class="w-5 h-5 inline text-green-600" />
+              Incomes
+            </h3>
+            <span class="ml-auto text-sm text-gray-400 font-normal">
+              +
+              <span class="font-bold text-green-600">
+                Rp{{ Number(dailyIncomeTotal).toLocaleString() }}
+              </span>
+            </span>
+          </div>
         </div>
-        <div class="w-px bg-gray-200 mx-4 hidden md:block" />
-        <div class="flex-1 pr-6">
-          <h3
-            class="text-lg font-semibold text-green-600 mb-4 flex items-center gap-2"
-          >
-            <ArrowDownTrayIcon class="w-5 h-5 inline text-green-600" />
-            Incomes
-            <span class="ml-auto text-sm text-gray-400 font-normal"
-              >+
-              <span class="font-bold text-green-600"
-                >Rp{{ Number(dailyIncomeTotal).toLocaleString() }}</span
-              ></span
+        <div
+          class="flex flex-col xl:flex-row gap-4 flex-1 pr-6 overflow-y-auto"
+        >
+          <div class="flex-1 flex flex-col">
+            <div
+              class="flex-1 items-center gap-2 flex justify-between xl:hidden"
             >
-          </h3>
-          <ul class="list-none p-0 m-0 divide-y divide-gray-100">
-            <li
-              v-for="item in dailyIncome"
-              :key="'income-' + item.id"
-              class="flex justify-between py-2 items-center"
-            >
-              <div>
-                <span class="text-gray-700 font-medium">{{
-                  item.nama_pemasukan
-                }}</span>
-                <span class="block text-xs text-gray-400">{{
-                  new Date(item.tanggal).toLocaleDateString()
-                }}</span>
-              </div>
-              <span class="text-green-600 font-bold"
-                >+ Rp{{ Number(item.jumlah_pemasukan).toLocaleString() }}</span
+              <h3
+                class="text-lg font-semibold text-red-500 flex items-center gap-2"
               >
-            </li>
-            <li
-              v-if="!dailyIncome.length"
-              class="py-2 text-gray-400 text-center"
+                <ArrowUpTrayIcon class="w-5 h-5 inline text-red-500" />
+                Expenses
+              </h3>
+              <span class="ml-auto text-sm text-gray-400 font-normal"
+                >-
+                <span class="font-bold text-red-500"
+                  >Rp{{ Number(dailyExpenseTotal).toLocaleString() }}</span
+                ></span
+              >
+            </div>
+            <ul class="list-none p-0 m-0 divide-y divide-gray-100 flex-1">
+              <li
+                v-for="item in dailyExpense"
+                :key="'expense-' + item.id"
+                class="flex justify-between py-2 items-center"
+              >
+                <div>
+                  <span class="text-gray-700 font-medium">{{
+                    item.nama_pengeluaran
+                  }}</span>
+                  <span class="block text-xs text-gray-400">{{
+                    new Date(item.tanggal).toLocaleDateString()
+                  }}</span>
+                </div>
+                <span class="text-red-500 font-bold"
+                  >- Rp{{
+                    Number(item.jumlah_pengeluaran).toLocaleString()
+                  }}</span
+                >
+              </li>
+              <li
+                v-if="!dailyExpense.length"
+                class="py-2 text-gray-400 text-center"
+              >
+                No expenses
+              </li>
+            </ul>
+          </div>
+          <div class="w-px bg-gray-200 mx-4 hidden xl:block" />
+          <div class="flex-1 flex flex-col">
+            <div
+              class="flex-1 items-center gap-2 flex justify-between xl:hidden"
             >
-              No incomes
-            </li>
-          </ul>
+              <h3
+                class="text-lg font-semibold text-green-600 flex items-center gap-2"
+              >
+                <ArrowDownTrayIcon class="w-5 h-5 inline text-green-600" />
+                Incomes
+              </h3>
+              <span class="ml-auto text-sm text-gray-400 font-normal">
+                +
+                <span class="font-bold text-green-600">
+                  Rp{{ Number(dailyIncomeTotal).toLocaleString() }}
+                </span>
+              </span>
+            </div>
+            <ul class="list-none p-0 m-0 divide-y divide-gray-100 flex-1">
+              <li
+                v-for="item in dailyIncome"
+                :key="'income-' + item.id"
+                class="flex justify-between py-2 items-center"
+              >
+                <div>
+                  <span class="text-gray-700 font-medium">{{
+                    item.nama_pemasukan
+                  }}</span>
+                  <span class="block text-xs text-gray-400">{{
+                    new Date(item.tanggal).toLocaleDateString()
+                  }}</span>
+                </div>
+                <span class="text-green-600 font-bold"
+                  >+ Rp{{
+                    Number(item.jumlah_pemasukan).toLocaleString()
+                  }}</span
+                >
+              </li>
+              <li
+                v-if="!dailyIncome.length"
+                class="py-2 text-gray-400 text-center"
+              >
+                No incomes
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
